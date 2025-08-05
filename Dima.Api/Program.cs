1096244082829ca27ca0241bec9aae285d346bfa
new-psam <marcelino.patricio@hotmail.com>
2025-08-05 08:36:04 -0300

    incluido teste API via swagger

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.AspNetCore.Components.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddScoped<Handler>(); // incluido via chat gpt
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+        x.CustomSchemaIds(type => type.FullName);
+});
+builder.Services.AddTransient<Handler>(); // incluido via Balta
+
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Endpoints -> URL para acesso
 app.MapPost(
 "/v1/transactions",
-        () => "Hello World!");
+        (Request request, Handler handler) 
+                => handler.Handle(request))
+    
+        .WithName("Transactions: Create")
+        .WithSummary("Create new Transaction")
+        .Produces<Response>();
 
 
 app.Run();
@@ -24,7 +43,29 @@ public class Request
         public string UserId { get; set; } = String.Empty;
 }
 
+// Response
+public class Response
+{
+        
+        public long Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+}
 
+// Handler
+public class Handler
+{
+        // Faz todo o processo de criação
+        // Persiste no banco...
+        public Response Handle(Request request)
+        {
+                return new Response
+                {
+                        Id = 4,
+                        Title = request.Title
+
+                };
+        }
+}
 
 
 
